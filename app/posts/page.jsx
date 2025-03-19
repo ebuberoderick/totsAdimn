b8a29752e0c614e5740e76a09afbd30061ce0390
ebuberoderick <ebuberoderick2@gmail.com>
2025-03-19 11:36:34 +0000

    updating post

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,17 +1,39 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AppLayout from '../components/layouts/appLayout'
 import PostCard from '../components/organisms/PostCard'
+import { fetchPosts } from '../services/authService'
+import AppPagination from '../components/organisms/AppPagination'
 
 function Page() {
+
+
+    const [list, setList] = useState([])
+
+    const fetchPostsFN = async () => {
+        const { data, status } = await fetchPosts()
+        if (status) {
+            setList(data.data)
+        }
+    }
+
+    useEffect(() => {
+        fetchPostsFN()
+    }, [])
+
     return (
         <AppLayout active={"Posts"} title={"Posts"}>
             <div className="grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
                 {
-                    // ["", "", "", "", "", ""].map((data, i) => (
-                    //     <PostCard key={i} />
-                    // ))
+                    list?.data?.map((data, i) => (
+                        <PostCard data={data} reload={fetchPostsFN} key={i} />
+                    ))
                 }
+            </div>
+
+            <div className="md:flex flex-wrap gap-8 md:items-center md:justify-between">
+                <div className="p-3 text-sm text-gray-400">Showing {list?.from} to {list?.to} of {list?.total} entries</div>
+                <AppPagination totalRecords={list} newData={(data) => setList(data)} />
             </div>
         </AppLayout>
     )
