ba55c1fd1fe9bd0e98836b6f341c05e8196bd11d
ebuberoderick <ebuberoderick2@gmail.com>
2025-03-20 22:11:00 +0000

    updated users Page added suspended

--- app/user_profiles/page.jsx
+++ app/user_profiles/page.jsx
@@ -2,17 +2,22 @@
 import React, { useEffect, useState } from 'react'
 import AppLayout from '../components/layouts/appLayout'
 import AppTable from '../components/organisms/AppTable'
-import { fetchUsers } from '../services/authService';
+import { fetchSuspendedUser, fetchUsers } from '../services/authService';
 import AppPagination from '../components/organisms/AppPagination';
 
 function Page() {
   const [users, setUsers] = useState(null);
-
+  const [active, setActive] = useState("all")
+  const [suspended, setSuspended] = useState(null)
+  const [all, setAll] = useState(null)
 
   const fetchUsersFN = async () => {
     const { data, status } = await fetchUsers()
-    if (status) {
+    const { data: res, status: sta } = await fetchSuspendedUser()
+    if (status && sta) {
       setUsers(data.data)
+      setAll(data.data)
+      setSuspended(res.data)
     }
   }
 
@@ -22,7 +27,10 @@ function Page() {
 
   return (
     <AppLayout active={"User Profiles"} title={"User Profiles"}>
-      <div className=""></div>
+      <div className="flex items-center gap-3">
+        <div onClick={() => {setActive("all");setUsers(all)}} className={`px-6 py-2 text-xs sm:text-sm rounded-md cursor-pointer ${active === "all" ? " bg-blue/80 hover:bg-blue text-white" : "bg-gray-50 hover:bg-gray-100"}`}>All</div>
+        <div onClick={() => {setActive("suspended");setUsers(suspended)}} className={`px-6 py-2 text-xs sm:text-sm rounded-md cursor-pointer ${active === "suspended" ? " bg-blue/80 hover:bg-blue text-white" : "bg-gray-50 hover:bg-gray-100"}`}>Suspended</div>
+      </div>
       <AppTable users={users?.data} />
       <div className="md:flex flex-wrap gap-8 md:items-center md:justify-between">
         <div className="p-3 text-sm text-gray-400">Showing {users?.from} to {users?.to} of {users?.total} entries</div>
