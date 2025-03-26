e09eeedeb1348c2cfce893504e32250b1b84bca6
ebuberoderick <ebuberoderick2@gmail.com>
2025-03-26 14:08:01 +0000

    updating table

--- app/components/organisms/PostTableChip.js
+++ app/components/organisms/PostTableChip.js
@@ -7,6 +7,7 @@ function PostTableChip({ data }) {
     const [post, setPost] = useState(data)
     const [proccessing, setProccessing] = useState(false)
 
+    console.log(data);
 
 
     const resolve = async () => {
@@ -20,7 +21,20 @@ function PostTableChip({ data }) {
 
     return (
         <tr className="border-b hover:bg-gray-50">
-            <td className="p-3 text-left w-80">Post</td>
+            <td className="p-3 text-left">
+                <div className='w-80'>
+                    <div className='flex gap-3 flex-wrap'>
+                    {
+                        data?.post?.image?.map((image, index) => (
+                            <div key={index} className="overflow-hidden mb-3">
+                                <Image src={image} width={100} height={100} className="w-12 h-12" alt="" />
+                            </div>
+                        ))
+                    }
+                    </div>
+                    <div>{data?.post?.text}</div>
+                </div>
+            </td>
             <td className="p-3 text-left">
                 <div className="flex w-72 items-center gap-3">
                     <div>
@@ -32,7 +46,7 @@ function PostTableChip({ data }) {
                 </div>
             </td>
             <td className="p-3 text-left"> <div className=" w-72">{post?.reason}</div></td>
-            <td className="p-3 text-left flex"><div className={`px-4 py-1 rounded-full text-xs ${post?.status === "unresolved"?"text-danger bg-danger/15":"text-blue bg-blue/15"}`}>{post?.status}</div></td>
+            <td className="p-3 text-left flex"><div className={`px-4 py-1 rounded-full text-xs ${post?.status === "unresolved" ? "text-danger bg-danger/15" : "text-blue bg-blue/15"}`}>{post?.status}</div></td>
             <td className="p-3 text-left">
                 {post?.status === "unresolved" && <div onClick={resolve} className={`px-6 py-2 inline text-xs sm:text-sm rounded-md cursor-pointer ${!proccessing ? " bg-blue/80 hover:bg-blue text-white" : "bg-gray-50 hover:bg-gray-100"}`}> {proccessing ? "Proccessing..." : "Resolve"}</div>}
             </td>
